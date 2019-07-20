8992d68607df472aeed33869d3cf99ea74e2df4e
Matthew Zuniga <mattz@mischiefcat.com>
2019-07-19 19:48:14 -0700

    Adding documentation

--- main.py
+++ main.py
@@ -1,13 +1,48 @@
-import flask
+"""
+Think of main.py as the "brain" of our project. It tells our web app where to get information and resources which are
+stored in the application structure.
+"""
 
-app = flask.Flask(__name__)
+# Imports
+import flask # Flask is a WGSI microframework for easy, lightweight, and scalable web applications.
 
-@app.route('/')
+app = flask.Flask(__name__) # Create the app object called "__name__"
+
+"""
+Flask apps begin in the root directory. They serve static HTML files in a directory called templates.
+In order to create more pages for our web app, create a new HTML file in the templates folder.
+
+Mess around with the files that are already inside the templates directory  to get a better understanding of how to
+create web pages.
+"""
+
+"""
+This is where we begin creating our Application Programming Interface (API).
+ - An API allows you to request resources from a server using REST. You should look up REST if you're not familiar.
+"""
+
+@app.route('/', methods=["GET"])
 def index():
+    """
+    -------------------------------------------  -------------------------------------------
+    @app.route() is a decorator which changes the behavior of the index() function.
+    @methods=["GET"] are the types of REST requests that the function takes. In this case, we only use GET which fetches resources.
+     -------------------------------------------  -------------------------------------------
+    @function index(): Renders index.html
+    @params: none
+    @returns: Renders index.html with two arguments for showing Jinja2 dynamic JavaScript.
+    -------------------------------------------  -------------------------------------------
+    """
     return flask.render_template('index.html', x=5, y=10)
 
-@app.route('/problem1.html/')
+@app.route('/problem1.html/', methods=["GET"])
 def problem1():
+    """
+    @function problem1(): Renders problem1.html
+    @params: none
+    @returns: Renders problem1.html.
+    -------------------------------------------  -------------------------------------------
+    """
     return flask.render_template('problem1.html')
 
 @app.after_request
